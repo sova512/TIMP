2822b9c33c6dd7a3c5bb882128f959deffdfa646
Gubanova OA 18pt202 <47846935+sova512@users.noreply.github.com>
2019-06-29 06:46:02 +0300

    Create laba1_modAlphaCipher.h

---
+++ laba1_modAlphaCipher.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+class modAlphaCipher
+{
+private:
+    wstring numAlpha = L"АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";  //алфавит по порядку
+    map <wchar_t,int> alphaNum;   //ассоциативный массив "номер по символу"
+    vector <int> key; //ключ
+    vector<int> convert(const wstring& s);//преобразование строка-вектор
+    wstring convert(const vector<int>& v);//преобразование вектор-строка
+public:
+    modAlphaCipher()=delete;//запретим конструктор без параметров
+    modAlphaCipher(const wstring& skey);//конструктор для установки ключа
+    wstring encrypt(const wstring& open_text);  //зашифрование
+    wstring decrypt(const wstring& cipher_text);//расшифрование
+};
